1e728875ea62204e397cc2f10a1546eca8cb4d26
Rocktim <Rocktim>
2020-03-07 00:44:10 +0530

    feat: sort by release date Added

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
     var promise = new Promise(function (resolve, reject) {
         $.getJSON('marvel_movie.json', function (json) {
             marvel_movie_list = json['items'];
+            marvel_movie_list.sort(function (a, b) { return new Date(a['release_date']) - new Date(b['release_date']) })
             resolve('Movie List')
         });
     });
@@ -31,12 +32,15 @@ $(document).ready(function () {
         var search = $("#searchVal").val();
         var results = []
 
+        var id = 0;
         $.each(marvel_movie_list, function (i, movie_list) {
             var result = fuzzyMatch(movie_list['title'], search);
             if (result) {
-                results.push('<tr><td>' + i + '</td><td>' + result + '</td><td>' + movie_list['release_date'] + '</td></tr>');
+                results.push('<tr><td>' + id + '</td><td>' + result + '</td><td>' + movie_list['release_date'] + '</td></tr>');
+                id++;
             }
         });
+
         var resultHTML = results.join('\n');
         $("#movieList").html(resultHTML);
     }
